010bc839f827ea8a314ff97e9296fbe6c7ab1097
Michael Nguyen <michaeltinh122@gmail.com>
2020-03-13 09:38:29 -0700

    ShotMeterATTEMPT

---
+++ Assets/PickupTest.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupTest : MonoBehaviour
+{
+
+    public GameObject bball;
+    private Rigidbody brb;
+
+    public bool pickable;
+    public bool holding;
+
+    public Transform theball;
+    public Transform thecamera;
+
+    public RigidbodyConstraints ogbrb;
+
+    public float throwspeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        brb = bball.GetComponent<Rigidbody>();
+        ogbrb = brb.constraints;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+
+
+        if (holding == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                brb.constraints = ogbrb;
+                brb.useGravity = true;
+                theball.parent = null;
+                brb.AddForce(thecamera.forward * throwspeed, ForceMode.Impulse);
+            }
+        }
+
+        if (pickable == true)
+        {
+            if (Input.GetKey(KeyCode.Mouse1))
+            {
+                StartCoroutine(pickingitup());
+            }
+        }
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            pickable = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            pickable = false;
+        }
+    }
+
+    public IEnumerator pickingitup()
+    {
+        brb.useGravity = false;
+        theball.SetParent(thecamera);
+        brb.velocity = new Vector3(0, 7, 0);
+        yield return new WaitForSeconds(.12f);
+        brb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY;
+        holding = true;
+    }
+
+
+}
---
+++ Assets/Scripts/ShootMeter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootMeter : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public GameObject ShootingMeter;
+    public GameObject ShootingFill;
+    public bool Shoot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+        ShootingMeter.GetComponent<Canvas>().enabled = false;
+        ShootingFill.GetComponent<Canvas>().enabled = false;
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            Shoot = true;
+        }
+        if (Shoot == true)
+        {
+            ShootingMeter.GetComponent<Canvas>().enabled = true;
+        }
+        if (Input.GetButton("E"))
+        {
+            ShootingFill.GetComponent<Canvas>().enabled = true;
+        }
+    }
+}
+
+
